73937cbc6b423b497a10b266f1e09b63d8d32171
wittybaji <wittybaji@gmail.com>
2023-05-24 18:23:33 +0800

    线程池内任务不同延迟方案测试

---
+++ ThreadPoolTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            int processorCount = Environment.ProcessorCount;
+            Console.WriteLine($"ProcessorCount：{processorCount}");
+            bool setMaxThreadsResult = ThreadPool.SetMaxThreads(processorCount, processorCount);
+            Console.WriteLine($"SetMaxThreads:{setMaxThreadsResult}");
+
+            ThreadPool.GetMaxThreads(out int workerThreads, out int completionPortThreads);
+            Console.WriteLine($"GetMaxThreads < {workerThreads} > < {completionPortThreads} >");
+            Thread addTask = new Thread(() =>
+            {
+                int taskId = 0;
+                while (true)
+                {
+                    taskId++;
+                    ThreadPool.GetAvailableThreads(out int availableWorkerThreads, out int availableCompletionPortThreads);
+                    Console.WriteLine($"GetAvailableThreads < {availableWorkerThreads} > < {availableCompletionPortThreads} >");
+                    bool queueResult;
+                    queueResult = ThreadPool.QueueUserWorkItem(p =>
+                    {
+                        Work(taskId);
+                    });
+                    //queueResult = ThreadPool.QueueUserWorkItem(async p =>
+                    //{
+                    //    await WorkAsync(taskId);
+                    //});
+                    if (queueResult)
+                    {
+                        Console.WriteLine($"++ {taskId}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"!! {taskId}");
+                    }
+                    Thread.Sleep(500);
+                }
+            });
+            addTask.Start();
+            Console.ReadLine();
+        }
+
+        private static async Task WorkAsync(int taskId)
+        {
+            Console.WriteLine($"-> 启动任务 {taskId} {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(10000);
+            Console.WriteLine($"<- 结束任务 {taskId} {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void Work(int taskId)
+        {
+            Console.WriteLine($"-> 启动任务 {taskId} {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(10000);
+            Console.WriteLine($"<- 结束任务 {taskId} {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
