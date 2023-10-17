eb5c549b09bf19273e000de1d4994d36c58a737d
wittybaji <wittybaji@gmail.com>
2023-10-17 17:19:45 +0800

    添加TimeSpan转换

---
+++ TimeSpanTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimeSpanTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string tsStr = default;
+                var ts = TimeSpan.FromSeconds(3600);
+                if (ts.Hours > 0)
+                {
+                     tsStr = ts.ToString(@"h\时m\分s\秒");
+                }
+                else
+                {
+                    tsStr = ts.ToString(@"m\分s\秒");
+                }
+                Console.WriteLine(tsStr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
